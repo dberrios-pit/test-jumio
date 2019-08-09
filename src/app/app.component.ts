a181f420af51273606bf9a1d0ecfe753f10536ee
Diego Berrios <diego@patagonia-it.com>
2019-08-09 16:52:02 -0400

    WIP: jumio en iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-jumio';
+  /* window.addEventListener('message', receiveMessage, false);
+  receiveMessage(event) {
+    var data = JSON.parse(event.data);
+    console.log(data);
+    console.log('Netverify Web was loaded in an iframe.');
+    console.log('auth token:', data.authorizationToken);
+    console.log('transaction reference:', data.transactionReference);
+    console.log('customer internal reference:', data.customerInternalReference);
+    console.log('event type:', data.eventType);
+    console.log('date-time:', data.dateTime);
+    console.log('event value:', data.payload.value);
+    console.log('event metainfo:', data.payload.metainfo);
+  } */
 }
